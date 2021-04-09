af6a04c0a1200a996c1592469a80ed6643dbc5bd
luuvanhung <luuhung200@gmail.com>
2021-04-09 12:12:12 +0700

    Update button changecolor

--- app/src/main/java/com/example/sinhvien/MainActivity.kt
+++ app/src/main/java/com/example/sinhvien/MainActivity.kt
@@ -1,26 +1,35 @@
 package com.example.sinhvien
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     val countrySource = listOf<CountryInfo>(
-        CountryInfo("Mai Tan Hung","11.11.2000","abc",R.drawable.profile),
-        CountryInfo("Nguyen Van Loc","02.11.2000","abc",R.drawable.loc_1),
-        CountryInfo("Nguyen Nhat Truong","1.12.2000","abc",R.drawable.videofilmer),
-        CountryInfo("Nguyen Tu Van","16.01.2000","abc",R.drawable.videofilmer),
-        CountryInfo("Nguyen Van Long","1.11.2000","abc",R.drawable.videofilmer)
+            CountryInfo("Mai Tan Hung", "11.11.2000", "abc", R.drawable.profile),
+            CountryInfo("Nguyen Van Loc", "02.11.2000", "abc", R.drawable.loc_1),
+            CountryInfo("Nguyen Nhat Truong", "1.12.2000", "abc", R.drawable.videofilmer),
+            CountryInfo("Nguyen Tu Van", "16.01.2000", "abc", R.drawable.videofilmer),
+            CountryInfo("Nguyen Van Long", "1.11.2000", "abc", R.drawable.videofilmer)
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val countryRecyclerView = findViewById<RecyclerView>(R.id.countryRV)
-        val countryAdapter = CountryRVAdapter(this,countrySource)
-        countryRecyclerView.layoutManager = GridLayoutManager(this,2)
+        val countryAdapter = CountryRVAdapter(this, countrySource)
+        countryRecyclerView.layoutManager = GridLayoutManager(this, 2)
         countryRecyclerView.adapter = countryAdapter
+        findViewById<Button>(R.id.changeColor_Btn).setOnClickListener {
+            countryRecyclerView.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
+        }
+        findViewById<Button>(R.id.backColor).setOnClickListener {
+            countryRecyclerView.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+        }
     }
-    //caiditconmemaythangchumbrand
 }
